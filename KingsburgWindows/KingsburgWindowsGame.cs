6f8859d66bddeffd1394be0fcf7789ed890cff98
Tyler Butler <tyler@tylerbutler.com>
2008-11-29 01:23:56 -0800

    Copied in files from the Marblets XNA sample.
    The Marblets sample is a good starting point I think. I'll take the base
    classes developed there and integrate the kingsburg logic around it.
    
    PROJECT DOES NOT CURRENTLY BUILD.

--- KingsburgWindows/KingsburgWindowsGame.cs
+++ KingsburgWindows/KingsburgWindowsGame.cs
@@ -11,6 +11,7 @@ using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
 using TylerButler.Kingsburg.Core;
+using Marblets;
 
 namespace TylerButler.Kingsburg.Windows
 {
@@ -19,13 +20,57 @@ namespace TylerButler.Kingsburg.Windows
     /// </summary>
     public class KingsburgWindowsGame : Microsoft.Xna.Framework.Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private InputHelper inputHelper;
+        private int previousWindowWidth = 1280;
+        private int previousWindowHeight = 720;
+
+        private MainGameScreen mainGameScreen;
+        private TitleScreen titleScreen;
+        private GameManager gm;
+
+
+        /// <summary>
+        /// A cache of content used by the game
+        /// </summary>
+        public new static ContentManager Content;
+
+        /// <summary>
+        /// The game settings from settings.xml
+        /// </summary>
+        //public static Settings Settings = new Settings();
+
+        /// <summary>
+        /// The storage device that the game is saving high-scores to.
+        /// </summary>
+        public static StorageDevice StorageDevice = null;
+
 
         public KingsburgWindowsGame()
         {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
+            graphics = new GraphicsDeviceManager( this );
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+
+            base.Content.RootDirectory = "Content";
+            KingsburgWindowsGame.Content = base.Content;
+
+            titleScreen = new TitleScreen( this, "Images/title_background.png", null );
+            titleScreen.Enabled = true;
+            titleScreen.Visible = true;
+            this.Components.Add( titleScreen );
+
+            mainGameScreen = new MainGameScreen( this, "Images/game_background.png", null );
+            mainGameScreen.Enabled = false;
+            mainGameScreen.Visible = false;
+            this.Components.Add( mainGameScreen );
+
+            inputHelper = new InputHelper( this );
+            this.Components.Add( inputHelper );
+
+            this.Components.Add( new GamerServicesComponent( this ) );
+
+            gm = GameManager.Instance;
         }
 
         /// <summary>
@@ -39,7 +84,6 @@ namespace TylerButler.Kingsburg.Windows
             // TODO: Add your initialization logic here
 
             base.Initialize();
-            GameManager gm = GameManager.Instance;
         }
 
         /// <summary>
@@ -48,9 +92,6 @@ namespace TylerButler.Kingsburg.Windows
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
             // TODO: use this.Content to load your game content here
         }
 
@@ -68,28 +109,55 @@ namespace TylerButler.Kingsburg.Windows
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
+        protected override void Update( GameTime gameTime )
         {
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if( GamePad.GetState( PlayerIndex.One ).Buttons.Back == ButtonState.Pressed )
                 this.Exit();
 
             // TODO: Add your update logic here
 
-            base.Update(gameTime);
+            base.Update( gameTime );
         }
 
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
+        protected override void Draw( GameTime gameTime )
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear( Color.CornflowerBlue );
 
             // TODO: Add your drawing code here
 
-            base.Draw(gameTime);
+            base.Draw( gameTime );
         }
+    }
+
+    /// <summary>
+    /// This enum is for the state transitions.
+    /// </summary>
+    public enum GameState
+    {
+        /// <summary>
+        /// Default value - means no state is set
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Nothing visible, game has just been run and nothing is initialized
+        /// </summary>
+        Started,
+
+        /// <summary>
+        /// Logo Screen is being displayed
+        /// </summary>
+        LogoSplash,
+
+        /// <summary>
+        /// Currently playing the 2d version
+        /// </summary>
+        GameInProgress,
+
     }
 }
---
+++ KingsburgWindows/TitleScreen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Marblets;
+
+namespace TylerButler.Kingsburg.Windows
+{
+    public class TitleScreen : Screen
+    {
+        public TitleScreen( Game game, string backgroundImage, SoundEntry backgroundMusic )
+            : base( game, backgroundImage, backgroundMusic )
+        {
+        }
+    }
+}
